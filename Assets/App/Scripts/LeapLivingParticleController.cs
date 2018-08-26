f15462d1c432abdedf4f2dc60b5e7c147b082bd6
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2018-08-27 02:27:57 +0900

    Added a new app.

---
+++ Assets/App/Scripts/LeapLivingParticleController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeapLivingParticleController : MonoBehaviour {
+
+    public Transform affector;
+    //public Transform eyes;
+    private ParticleSystemRenderer psr;
+
+	void Start () {
+        psr = GetComponent<ParticleSystemRenderer>();
+	}
+	
+	void Update () {
+        setAffectorPosition();
+    }
+
+    private void setAffectorPosition()
+    {
+        if (this.affector != null) {
+            if (Input.GetKey(KeyCode.W)) {
+                this.affector.position += new Vector3(0, 0, 0.02f);
+            }
+            else if (Input.GetKey(KeyCode.S)) {
+                this.affector.position += new Vector3(0, 0, -0.02f);
+            }
+            else if (Input.GetKey(KeyCode.D)) {
+                this.affector.position += new Vector3(0.02f, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.A)) {
+                this.affector.position += new Vector3(-0.02f, 0, 0);
+            }
+            /*
+            else if (Input.GetKey(KeyCode.J)) {
+                if (Input.GetKey(KeyCode.LeftShift)) {
+                    this.eyes.Rotate(new Vector3(-0.02f, 0, 0));
+                } else {
+                    this.eyes.Rotate(new Vector3(0.02f, 0, 0));
+                }
+            }
+            else if (Input.GetKey(KeyCode.K)) {
+                if (Input.GetKey(KeyCode.LeftShift)) {
+                    this.eyes.Rotate(new Vector3(0, -0.02f, 0));
+                } else {
+                    this.eyes.Rotate(new Vector3(0, 0.02f, 0));
+                }
+            }
+            else if (Input.GetKey(KeyCode.L)) {
+                if (Input.GetKey(KeyCode.LeftShift)) {
+                    this.eyes.Rotate(new Vector3(0, 0, -0.02f));
+                } else {
+                    this.eyes.Rotate(new Vector3(0, 0, 0.02f));
+                }
+            }
+            */
+            this.psr.material.SetVector("_Affector", this.affector.position);
+        }
+    }
+}
---
+++ Assets/Plugin/SineVFX/LivingParticles/Resources/Scripts/ParticleSystemRampGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemRampGenerator : MonoBehaviour {
+
+    public Gradient procedrualGradientRamp;
+    public bool procedrualGradientEnabled = false;
+    public bool updateEveryFrame = false;
+
+    private ParticleSystemRenderer psr;
+    private Texture2D rampTexture;
+
+	void Start () {
+        psr = GetComponent<ParticleSystemRenderer>();
+
+        if (procedrualGradientEnabled == true)
+        {
+            UpdateRampTexture();
+        }
+    }
+
+    void Update () {
+        if (procedrualGradientEnabled == true)
+        {
+            if (updateEveryFrame == true)
+            {
+                UpdateRampTexture();
+            }
+        }
+    }
+
+    // Generating a texture from gradient variable
+    Texture2D GenerateTextureFromGradient(Gradient grad)
+    {
+        float width = 256;
+        float height = 1;
+        Texture2D text = new Texture2D((int)width, (int)height);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color col = grad.Evaluate(0 + (x / width));
+                text.SetPixel(x, y, col);
+            }
+        }
+        text.wrapMode = TextureWrapMode.Clamp;
+        text.Apply();
+        return text;
+    }
+
+    // Update procedural ramp textures and applying them to the shaders
+    public void UpdateRampTexture()
+    {
+        rampTexture = GenerateTextureFromGradient(procedrualGradientRamp);
+        psr.material.SetTexture("_Ramp", rampTexture);
+    }
+}
